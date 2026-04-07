bee6624e2c09bd43c53956cb74a4162d81ab0c1a
23520432 <23520432@gm.uit.edu.vn>
2026-04-08 00:25:52 +0700

    Sua chuc nang loaidocgia

--- library/src/main/java/com/lms/library/dto/request/DocGiaCreationRequest.java
+++ library/src/main/java/com/lms/library/dto/request/DocGiaCreationRequest.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class DocGiaRequest {
+public class DocGiaCreationRequest {
 
     Integer maLoaiDocGia;
     String hoTen;
