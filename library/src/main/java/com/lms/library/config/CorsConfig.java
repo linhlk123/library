609982aa2f296a1cbab635ec4a08e7a5bee620c9
KHANH LINH <linhluutk@gmail.com>
2026-05-10 08:08:29 +0700

    fix: change CORS 1

--- library/src/main/java/com/lms/library/config/CorsConfig.java
+++ library/src/main/java/com/lms/library/config/CorsConfig.java
@@ -14,6 +14,8 @@
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowCredentials(true);
             config.addAllowedOrigin("https://library-fe-five.vercel.app"); 
+            config.addAllowedOrigin("http://localhost:5173");
+            config.addAllowedOrigin("http://localhost:5174");
             config.addAllowedHeader("*");
             config.addAllowedMethod("*");
 
